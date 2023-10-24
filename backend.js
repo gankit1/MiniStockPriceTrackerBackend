a512e1a5ee0a8c1bc3fbf2057f43f3f0b72ddd53
smeer.mishra98 <smeer.mishra98@gmail.com>
2023-10-24 14:14:36 +0530

    new

--- backend.js
+++ backend.js
@@ -33,7 +33,7 @@ app.get('/api/stock_prices', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
